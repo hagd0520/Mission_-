6ea2f60f3f71a48529cdbef85089ae73873c3325
hagd <hagd0520@gmail.com>
2023-11-06 11:50:43 +0900

    5 단계 목록

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,13 +16,13 @@ public class App {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
-            System.out.print("명령)");
+            System.out.print("명령) ");
             String cmd = sc.nextLine();
             Rq rq = new Rq(cmd, 0);
 
             if (rq.cmd.equals("종료")) break;
 
-            // 2 단계
+            // 2, 3, 4 단계
             if (rq.cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = sc.nextLine();
@@ -31,13 +33,26 @@ public class App {
                 System.out.printf("%d번 명언이 등록되었습니다.\n", idQueue);
                 idQueue++;
             }
+
+            // 5 단계
+            if (rq.cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언\n-----------------------");
+                for (int i = quotes.size() - 1; i >= 0; i--) {
+                    Quote quote = quotes.get(i);
+
+                    System.out.printf("%d / %s / %s\n", quote.getId(), quote.getAuthor(), quote.getContent());
+                }
+            }
         }
     }
 }
 
 class Quote {
+    @Getter
     private int id;
+    @Getter
     private String content;
+    @Getter
     private String author;
     public Quote(int id, String content, String author) {
         this.id = id;
@@ -52,7 +67,7 @@ class Rq {
     String id;
     public Rq(String cmd, int defaultNum) {
         String[] cmds = cmd.split("\\?");
-        if (cmds.length == 1) this.cmd = cmds[0];
+        if (cmds.length == 1) this.cmd = cmds[0].trim();
         if (cmds.length > 1) {
             this.cmd = cmds[0].trim();
             if (cmds.length > 1) {
