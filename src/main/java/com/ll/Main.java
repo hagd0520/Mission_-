7fb6e709b2f8c0e688c2f3ac8112d8e3efb2689b
hagd <hagd0520@gmail.com>
2023-11-08 16:12:52 +0900

    병합커밋

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,15 +1,9 @@
 package com.ll;
 
-<<<<<<< HEAD
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         App.run();
-=======
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
->>>>>>> bb42426d108a2d151e4527c561a290318f61b5fb
     }
 }
